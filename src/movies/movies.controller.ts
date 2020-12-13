e02f081f0145277ed88dc78c9e2972c6d453f1ce
yeo311 <yeojihoon311@gmail.com>
2020-12-13 23:01:30 +0900

    add movies service

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movies.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+
+    constructor(private readonly moviesService: MoviesService) { }
+
+    @Get()
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
+    }
+
+    @Get(":id")
+    getOne(@Param('id') id: string): Movie {
+        return this.moviesService.getOne(id);
+    }
+
+    @Post()
+    create(@Body() movieData) {
+        return this.moviesService.create(movieData);
+    }
+
+    @Delete(":id")
+    delete(@Param('id') id: string) {
+        return this.moviesService.deleteOne(id);
+    }
+
+    @Patch(":id")
+    patch(@Param('id') id: string, @Body() updateData) {
+        return {
+            updatedID: id,
+            ...updateData,
+        };
+    }
+
+}
